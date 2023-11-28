9710dbafb493c46c91f65a6d66e31795f2fd9312
Ren <theradar3@gmail.com>
2023-11-29 02:46:00 +0300

    Create 605. Can Place Flowers.py

---
+++ Leetcode/Python/605. Can Place Flowers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    flowerbed = [1, 0, 0, 0, 1]
+    n = 2
+    def canPlaceFlowers(flowerbed, n):
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
+    print(canPlaceFlowers(flowerbed, n))
+
